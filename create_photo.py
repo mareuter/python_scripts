eedcfcfc46a35ec33e265f3620994692648aa7b1
Michael Reuter <mareuternh@gmail.com>
2017-12-07 21:30:45 -0700

    Adding create_photo.py script.

---
+++ create_photo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+# Script must be run from Pictures directory on NAS
+
+try:
+    year = sys.argv[1]
+except IndexError:
+    print("Usage: create_photo.py <year>")
+    sys.exit(255)
+
+os.mkdir(year)
+os.chdir(year)
+
+DIR_LIST = ["AstroImaging", "Astronomy", "Astronomy_Raw", "Astronomy_Tif", "Other", "Panorama", "Photos"]
+for pdir in DIR_LIST:
+   os.mkdir(pdir)
+
+for i in range(12):
+    os.mkdir(os.path.join("Photos", "{:02d}".format(i + 1)))
