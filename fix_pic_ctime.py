5aa9d3c98aae132a5200047fda503a10fb66cbff
Michael Reuter <mareuternh@gmail.com>
2017-12-22 16:06:52 -0700

    Adding fix_pic_ctime script.

---
+++ fix_pic_ctime.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+import pyexifinfo
+import optparse
+import os
+import sys
+
+VERSION="1.0.0"
+
+def make_description():
+    descr = []
+    descr.append("This script determines the creation time from a picture's")
+    descr.append("EXIF information and adjusts the picture's mtime")
+    descr.append("attribute.")
+    return " ".join(descr)
+
+def check_filename(filename):
+    return filename.lower().endswith('.jpg') or \
+           filename.lower().endswith('.tif') or \
+           filename.lower().endswith('.cr2') or \
+           filename.lower().endswith('.orf')
+
+def run(opts):
+    for pfile in os.listdir('.'):
+        if os.path.isfile(pfile) and check_filename(pfile):
+            if opts.debug:
+                print(pfile)
+
+            try:
+                im = pyexifinfo.information(pfile)
+                pic_time = im["EXIF:DateTimeOriginal"]
+                dt = datetime.strptime(pic_time, "%Y:%m:%d %H:%M:%S")
+
+                stinfo = os.stat(pfile)
+                os.utime(pfile, (stinfo.st_atime, dt.timestamp()))
+            except KeyError:
+                pass
+
+if __name__ == "__main__":
+    parser = optparse.OptionParser("usage: %prog [options]",
+                                   version=VERSION,
+                                   description=make_description())
+
+    parser.add_option("-d", "--debug", dest="debug", action="store_true",
+                      help="Debug the program.")
+    parser.set_defaults(debug=False)
+
+    (options, args) = parser.parse_args()
+    run(options)
