6209bbe5868659f8ebafc897864a0202b4018d62
Michael Reuter <mareuternh@gmail.com>
2018-01-12 20:45:29 -0700

    Updates to create_photo.py
     * Split locations for photo and Picture_Backup directories

--- pictures/create_photo.py
+++ pictures/create_photo.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
+# Script must be run in a place where the NAS directories photo and
+# Picture_Backup are mounted
+
 import os
 import sys
 
-# Script must be run from Pictures directory on NAS
+DIR_LIST = ["AstroImaging", "Astronomy_Raw", "Astronomy_Tif", "Other", "Panorama"]
+MAIN_DIR = "photo"
+BACKUP_DIR = "Picture_Backup"
 
 try:
     year = sys.argv[1]
@@ -11,12 +16,20 @@ except IndexError:
     print("Usage: create_photo.py <year>")
     sys.exit(255)
 
+if not (os.path.exists(MAIN_DIR) and os.path.exists(BACKUP_DIR)):
+    print("Please change to the location where photo and Picture_Backup are both mounted.")
+    sys.exit(254)
+
+os.chdir(BACKUP_DIR)
 os.mkdir(year)
 os.chdir(year)
 
-DIR_LIST = ["AstroImaging", "Astronomy", "Astronomy_Raw", "Astronomy_Tif", "Other", "Panorama", "Photos"]
 for pdir in DIR_LIST:
     os.mkdir(pdir)
 
+os.chdir(os.path.join("../../", MAIN_DIR))
+os.mkdir(year)
+os.chdir(year)
+
 for i in range(12):
-    os.mkdir(os.path.join("Photos", "{:02d}".format(i + 1)))
+    os.mkdir("{:02d}".format(i + 1))
