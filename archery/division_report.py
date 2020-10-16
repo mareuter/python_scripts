cbccffcf3f9a5f0df31726a8dd4b2243a167f65a
Michael Reuter <mareuternh@gmail.com>
2020-10-15 20:52:40 -0700

    Update archery script for changes due to latest tournament.

--- archery/division_report.py
+++ archery/division_report.py
@@ -3,16 +3,19 @@ from dataclasses import dataclass
 from datetime import datetime, timedelta
 from operator import attrgetter
 import os
+import re
 import subprocess
 import sys
 
 from openpyxl import load_workbook
 
-# Map the spreadsheet columns
-FIRST_NAME = 3
-LAST_NAME = 4
-MENS_DIVISION = 48
-WOMENS_DIVISION = 49
+FIRST_NAME_CELL = "First Name"
+LAST_NAME_CELL = "Last Name"
+DATE_OF_BIRTH_CELL = "DOB"
+MENS_DIVISION_CELL = "Men's Divisions"
+WOMENS_DIVISION_CELL = "Women's Divisions"
+
+DIGITS_IN_DIVISION = re.compile(r"\d")
 
 
 # Archer class
@@ -38,10 +41,21 @@ DIVISION_CLASS_ORDER = [
     "Yeoman",
 ]
 
-
+AGE_DATE_FORMAT = "%Y-%m-%d"
 DATE_INPUT_FORMAT = "%Y/%m/%d"
 DATE_OUTPUT_FORMAT = "%B %d, %Y"
 DATE_FILE_FORMAT = "%Y%m%d"
+
+
+def masters_age_bracket(age):
+    if age < 50:
+        return None
+    elif age >= 50 and age < 60:
+        return 50
+    elif age >= 60 and age < 70:
+        return 60
+    else:
+        return 70
 
 
 def main(opts):
@@ -50,8 +64,27 @@ def main(opts):
     except IndexError:
         print("Usage: division_report.py <Input Spreadsheet>")
 
+    if opts.date is None:
+        tournament_date = datetime.now()
+    else:
+        tournament_date = datetime.strptime(opts.date, DATE_INPUT_FORMAT)
+
     workbook = load_workbook(ifile)
     sheet = workbook.active
+
+    # Find column indexes
+    title_row = list(sheet.iter_rows(min_row=2, max_row=2, values_only=True))[0]
+    FIRST_NAME = title_row.index(FIRST_NAME_CELL)
+    LAST_NAME = title_row.index(LAST_NAME_CELL)
+    DATE_OF_BIRTH = title_row.index(DATE_OF_BIRTH_CELL)
+    MENS_DIVISION = title_row.index(MENS_DIVISION_CELL)
+    WOMENS_DIVISION = title_row.index(WOMENS_DIVISION_CELL)
+
+    if opts.masters_age:
+        DIVISION_CLASS_ORDER.pop(0)
+        DIVISION_CLASS_ORDER.insert(0, "Master 50")
+        DIVISION_CLASS_ORDER.insert(0, "Master 60")
+        DIVISION_CLASS_ORDER.insert(0, "Master 70")
 
     archers = []
     for row in sheet.iter_rows(min_row=3, values_only=True):
@@ -67,9 +100,22 @@ def main(opts):
             division = row[WOMENS_DIVISION]
 
         if "(" in division:
-            divison_bow, _, divison_class = division.split()
+            if DIGITS_IN_DIVISION.search(division) is not None:
+                divison_bow, _, divison_class, _ = division.split()
+            else:
+                divison_bow, _, divison_class = division.split()
+        elif DIGITS_IN_DIVISION.search(division) is not None:
+            divison_bow, divison_class, _ = division.split()
         else:
             divison_bow, divison_class = division.split()
+
+        if opts.masters_age:
+            dob_date = datetime.strptime(row[DATE_OF_BIRTH], AGE_DATE_FORMAT)
+            age_cutoff_date = datetime(tournament_date.year, 1, 1)
+            age_at_tournament = age_cutoff_date.year - dob_date.year
+            age_bracket = masters_age_bracket(age_at_tournament)
+            if age_bracket is not None:
+                divison_class = f"{divison_class} {age_bracket}"
 
         archers.append(
             Archer(
@@ -86,11 +132,6 @@ def main(opts):
     else:
         tournament = opts.tournament
 
-    if opts.date is None:
-        tournament_date = datetime.now()
-    else:
-        tournament_date = datetime.strptime(opts.date, DATE_INPUT_FORMAT)
-
     if opts.ndays > 1:
         tournament_end_date = tournament_date + timedelta(days=opts.ndays - 1)
     else:
@@ -102,22 +143,26 @@ def main(opts):
     report_lines.append("..." + os.linesep)
     if tournament_end_date is None:
         formatted_date = tournament_date.strftime(DATE_OUTPUT_FORMAT)
-    if tournament_end_date.month == tournament_date.month:
-        start_parts = tournament_date.strftime(DATE_OUTPUT_FORMAT).split()
-        end_parts = tournament_end_date.strftime(DATE_OUTPUT_FORMAT).split()
-        formatted_date = f"{start_parts[0]} {start_parts[1].strip(',')}-{end_parts[1]} {start_parts[2]}"
-    if tournament_end_date.month > tournament_date.month:
-        start_parts = tournament_date.strftime(DATE_OUTPUT_FORMAT).split()
-        end_parts = tournament_end_date.strftime(DATE_OUTPUT_FORMAT).split()
-        formatted_date = (
-            f"{start_parts[0]} {start_parts[1].strip(',')}-"
-            "{end_parts[0]} {end_parts[1]} {start_parts[2]}"
-        )
+    else:
+        if tournament_end_date.month == tournament_date.month:
+            start_parts = tournament_date.strftime(DATE_OUTPUT_FORMAT).split()
+            end_parts = tournament_end_date.strftime(DATE_OUTPUT_FORMAT).split()
+            formatted_date = f"{start_parts[0]} {start_parts[1].strip(',')}-{end_parts[1]} {start_parts[2]}"
+        if tournament_end_date.month > tournament_date.month:
+            start_parts = tournament_date.strftime(DATE_OUTPUT_FORMAT).split()
+            end_parts = tournament_end_date.strftime(DATE_OUTPUT_FORMAT).split()
+            formatted_date = (
+                f"{start_parts[0]} {start_parts[1].strip(',')}-"
+                "{end_parts[0]} {end_parts[1]} {start_parts[2]}"
+            )
     report_lines.append(f"{formatted_date}" + os.linesep)
     report_lines.append(os.linesep)
 
     report_lines.append(f"Number of Registered Archers: {len(archers)}" + os.linesep)
     report_lines.append(os.linesep)
+
+    if opts.reverse:
+        DIVISION_TYPE_ORDER.reverse()
 
     for dto in DIVISION_TYPE_ORDER:
         for dbo in DIVISION_BOW_ORDER:
@@ -182,6 +227,21 @@ if __name__ == "__main__":
         type=int,
         default=1,
         help="Number of days for the tournament.",
+    )
+
+    parser.add_argument(
+        "-r",
+        "--reverse",
+        dest="reverse",
+        action="store_true",
+        help="Reverse order of division type.",
+    )
+
+    parser.add_argument(
+        "--masters-age",
+        dest="masters_age",
+        action="store_true",
+        help="Sort Master's divisions based on age.",
     )
 
     parser.add_argument(
