f1faceee708d46218a73ea0dd257ab8f8de7f474
Michael Reuter <mareuternh@gmail.com>
2023-09-20 20:01:04 -0700

    Change create_mapper to handle new JSON map file format.

--- dosgames/create_mapper.py
+++ dosgames/create_mapper.py
@@ -4,16 +4,25 @@ import os
 import pathlib
 
 
+def find_mapping(key: str, mapping: dict, controller: str) -> list | None:
+    cmap = None
+    for key_map in mapping:
+        if key_map["key"]["name"] == key:
+            cmap = key_map["key"][controller]
+            break
+    return cmap
+
+
 def main(opts: argparse.Namespace) -> None:
     with opts.mapper.open() as mfile:
         mapper_lines = mfile.readlines()
     controller_config = json.loads(pathlib.Path(f"{opts.controller}.json").read_text())
-    game_key_map = json.loads(opts.game_mapping.read_text())[opts.controller] 
+    game_key_map = json.loads(opts.game_mapping.read_text())["actions"]
 
     new_mapper = opts.mapper.parent / opts.controller / opts.game_mapping.name
     new_mapper = new_mapper.with_suffix(".map")
 
-    game_keys = list(game_key_map.keys())
+    game_keys = [x["key"]["name"] for x in game_key_map]
 
     game_mapper = []
     for mapper_line in mapper_lines:
@@ -23,7 +32,7 @@ def main(opts: argparse.Namespace) -> None:
             map_key = mapper_line.strip()
         if map_key in game_keys:
             new_mapper_line = [map_key]
-            controller_map = game_key_map[map_key]["map"]
+            controller_map = find_mapping(map_key, game_key_map, opts.controller)
             for value in controller_map:
                 new_mapper_line.append(f'"{controller_config[value]}"')
             new_mapper_line.append(values)
@@ -34,12 +43,15 @@ def main(opts: argparse.Namespace) -> None:
     new_mapper.parent.mkdir(parents=True, exist_ok=True)
     new_mapper.write_text("".join(game_mapper))
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("controller", help="The controller for mapping.")
     parser.add_argument("mapper", type=pathlib.Path, help="Blank mapping file.")
-    parser.add_argument("game_mapping", type=pathlib.Path, help="Key mapping file for game.")
+    parser.add_argument(
+        "game_mapping", type=pathlib.Path, help="Key mapping file for game."
+    )
 
     args = parser.parse_args()
     main(args)
