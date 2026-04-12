14d77971cfc4c89bf2c6c84e3877a13b68e7bbab
Michael Reuter <mareuternh@gmail.com>
2026-04-12 15:02:47 -0700

    Changes to windows runner.

--- dosgames/run_windows.py
+++ dosgames/run_windows.py
@@ -1,5 +1,4 @@
 import argparse
-import json
 import os
 import pathlib
 import subprocess as sp
@@ -7,36 +6,44 @@ import subprocess as sp
 TOP_DIR = pathlib.Path("~/DosGames").expanduser()
 CONFIG_DIR = TOP_DIR / "config"
 RUNNER = "flatpak run com.dosbox_x.DOSBox-X"
-MEDIA_OPTIONS = {
-    "floppy": "-t floppy",
-    "cdrom": "-t iso"
-}
+FLOPPY_525 = "-fs none -t floppy -size 512,15,2,80"
+FLOPPY_35 = "-t floppy"
+IMAGE_DIR = TOP_DIR / "windows"
+HARD_DRIVE_PARS = "-ide 1m"
+CDROM = "empty -t iso -ide 2m"
+WINDOWS_UPDATES = TOP_DIR / "windows" / "updates"
 
 
 def main(opts: argparse.Namespace) -> None:
+    boot_config = []
+    boot_conf_file = pathlib.Path("boot.conf")
 
-    media_config = []
-    media_conf_file = None
-    if opts.media is not None:
-        with opts.media.open() as ifile:
-            media = json.load(ifile)
-            media_conf_file = pathlib.Path("media.conf")
-            with media_conf_file.open("w") as ofile:
-                ofile.write("[autoexec]" + os.linesep)
-                for item in media["media"]:
-                    media_drive = item["drive"]
-                    media_type = item["type"]
-                    media_option = MEDIA_OPTIONS[media_type]
-                    media_list = " ".join([str(TOP_DIR / media_type / x) for x in item["files"]])
-                    media_mount = f"IMGMOUNT {media_drive} {media_list} {media_option}"
-                    ofile.write(media_mount + os.linesep)
+    with boot_conf_file.open("w") as ofile:
+        ofile.write("[autoexec]" + os.linesep)
+        filine = f"{IMAGE_DIR / f'floppy{opts.floppy}.img'}"
+        if opts.floppy == "35":
+            fline = f"IMGMOUNT A {filine} {FLOPPY_35}"
+        else:
+            fline = f"IMGMOUNT A {filine} {FLOPPY_525}"
+        ofile.write(fline + os.linesep)
+        himg = IMAGE_DIR / f"{opts.windows}_hdd.img"
+        cline = f"IMGMOUNT C {himg} {HARD_DRIVE_PARS}"
+        ofile.write(cline + os.linesep)
+        if opts.updates:
+            dline = f"MOUNT D {WINDOWS_UPDATES / opts.windows}"
+            ofile.write(dline + os.linesep)
+        cd_drive = "E" if opts.updates else "D"
+        cd_line = f"IMGMOUNT {cd_drive} {CDROM}"
+        ofile.write(cd_line + os.linesep)
+        bline = "BOOT C:"
+        ofile.write(bline + os.linesep)
 
-        media_config.append("-conf")
-        media_config.append(media_conf_file.name)
+    boot_config.append("-conf")
+    boot_config.append(boot_conf_file.name)
 
     cmd = RUNNER.split()
 
-    cmd.extend(media_config)
+    cmd.extend(boot_config)
     cmd.append("-conf")
     cmd.append(str(CONFIG_DIR / f"{opts.windows}.conf"))
 
@@ -48,16 +55,18 @@ def main(opts: argparse.Namespace) -> None:
         for line in proc.stdout.decode().split(os.linesep):
             print(line)
 
-    if media_conf_file is not None:
-        media_conf_file.unlink()
+    if boot_conf_file is not None:
+        boot_conf_file.unlink()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("windows", help="The flavor of windows to run.")
-    parser.add_argument("-m", "--media", type=pathlib.Path, help="Specify a file with media to mount.")
     parser.add_argument("--debug", dest="debug", action="store_true")
+    parser.add_argument("-f", "--floppy", choices=["525", "35"], default="35",
+                        help="Set the floppy drive size.")
+    parser.add_argument("-u", "--updates", action="store_true", help="Mount the area for Windows updates.")
 
     args = parser.parse_args()
     main(args)
